4314c2369fa442484d254824c79cc1b5d15a91de
limon <daniel.campos@est.fi.uncoma.edu.ar>
2022-10-21 17:42:37 -0300

    cripto

--- src/Criptografia/Criptografia.java
+++ src/Criptografia/Criptografia.java
@@ -2,11 +2,11 @@ package Criptografia;
 
 public class Criptografia {
 
-    public static int expoDyV(int a, int n) {
+    public static long expoDyV(long a, long n) {
         if (n == 1) {
             return a;
         } else if (n % 2 == 0) {
-            int b = expoDyV(a, n / 2);
+            long b = expoDyV(a, n / 2);
             return multiplicarDyV(b, b);
         } else {
             return multiplicarDyV(a, expoDyV(a, n - 1));
@@ -19,8 +19,8 @@ public class Criptografia {
     }
 
     public static String sumaString(String a, String b) {
-        int c = Integer.parseInt(a) + Integer.parseInt(b);
-        return Integer.toString(c);
+        Long c = Long.parseLong(a) + Long.parseLong(b);
+        return Long.toString(c);
     }
 
     public static String splitString(String cadena, int inicio, int fin) {
@@ -32,19 +32,19 @@ public class Criptografia {
         }
     }
 
-    public static int multiplicarDyV(int a, int b) {
-        return karatsuba(Integer.toString(a), Integer.toString(b));
+    public static long multiplicarDyV(long a, long b) {
+        return karatsuba(Long.toString(a), Long.toString(b));
     }
 
-    public static int karatsuba(String a, String b) {
+    public static long karatsuba(String a, String b) {
         // System.out.println("......A: "+a);
         // System.out.println("......B: "+b);
         String AL, AR, BL, BR;
-        int X1, X2, X3;
+        long X1, X2, X3;
 
         int n = Math.max(a.length(), b.length());
         if (n == 1) {
-            return Integer.parseInt(a) * Integer.parseInt(b);
+            return Long.parseLong(a) * Long.parseLong(b);
         } else {
             AL = splitString(a, 0, a.length() / 2);
             // System.out.println("AL: "+AL);
@@ -58,15 +58,40 @@ public class Criptografia {
             X2 = karatsuba(sumaString(AL, AR), sumaString(BL, BR));
             X3 = karatsuba(AR, BR);
 
-            return (int) (X1 * Math.pow(10, n) + (X2 - X1 - X3) * Math.pow(10, n / 2) + X3);
+            return  (long) (X1 *(Math.pow(10, n)) + (X2 - X1 - X3) * (Math.pow(10, n / 2)) + X3);
         }
 
+    }
+
+    public static int exponentModDyV(int a, int n, int z) {
+
+        // Casos bases
+        if (a == 0)
+            return 0;
+        if (n == 0)
+            return 1;
+
+        // Si n es par
+        long r;
+        if (n % 2 == 0) {
+            r = exponentModDyV(a, n / 2, z);
+            r =  (multiplicarDyV(r, r)) % z;
+        }
+
+        // Si n es impar
+        else {
+            r = a % z;
+            r = ( multiplicarDyV(r, exponentModDyV(a, n - 1, z)) % z) % z;
+        }
+
+        return (int) ((r + z) % z);
     }
 
     public static void main(String[] args) {
 
         // System.out.println(expoDyV(3, 4));
-        System.out.println(multiplicarDyV(1980, 2315));
+        //System.out.println(multiplicarDyV(1980, 2315));
+        System.out.println(exponentModDyV(211, 2, 5));
         // String test=Integer.toString(1970);
         // System.out.println(test);
         /*
